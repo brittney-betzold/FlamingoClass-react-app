351cc9b43e77094f01fcd2f05eb0251896fb43a6
Brittney <brittney.betzold2020@gmail.com>
2023-09-22 15:57:59 -0500

    Lesson_1.3

--- flamingoclass-react-todo/src/AddTodoForm.js
+++ flamingoclass-react-todo/src/AddTodoForm.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTodoForm() {
-    return(
-        <form>
-            <label htmlFor="todoTitle">Title</label>
-            <input id = "todoTitle" type = "text" name = "todoTitle"></input>
-            <button type = "submit">Add</button>
-        </form>
-    )
+function AddTodoForm(props) {
+
+const [todoTitle, setTodoTitle] = useState("");
+
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+          console.log("Todo Title:", todoTitle)
+          props.onAddTodo(todoTitle)
+          setTodoTitle("")
+      
+    };
+    // clears value of text input
+    const handleTitleChange = (event) => {
+        setTodoTitle(event.target.value);
+    }
+
+        return(
+            <form onSubmit = {handleAddTodo}>
+                <label htmlFor="todoTitle">Title</label>
+                <input id = "todoTitle" 
+                     type = "text" 
+                     name = "title" 
+                     value = {todoTitle}
+                     onChange= {handleTitleChange}></input>
+                <button type = "submit">Add</button>
+            </form>
+        )
+
 }
 
 export default AddTodoForm;

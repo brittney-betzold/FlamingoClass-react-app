b5f259077c7d1898eb6d3b0267ef2defc871cab0
Brittney <brittney.betzold2020@gmail.com>
2023-10-18 16:17:00 -0500

    lesson-1-4

--- flamingoclass-react-todo/src/TodoList.js
+++ flamingoclass-react-todo/src/TodoList.js
@@ -1,24 +1,18 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
-const todoList = [
-    { id: 1, title: "dishes" },
-    {
-      id: 2,
-      title: "school assignment",
-    },
-    {
-      id: 3,
-      title: "work",
-    },
-  ];
-function TodoList() {
-        return(
-            <ul>
-            {todoList.map((todo ) => (
-                <TodoListItem key = {todo.id} todo = {todo} />
-            ))}
-          </ul>
-        )
+
+function TodoList({ todoList }) {
+  const listStyle = {
+    textAlign: "center", // Center-align the items within the TodoList
+  };
+
+  return (
+    <ul style={listStyle}>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
--- flamingoclass-react-todo/src/index.js
+++ flamingoclass-react-todo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import reportWebVitals from './reportWebVitals'
 
 const root = document.getElementById("root");
 const rootElement = ReactDOM.createRoot(root);
@@ -12,3 +13,4 @@ rootElement.render(<App />);
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+// reportWebVitals();
